0753b720723a569b6ecb98595822043fe215b1e7
PhucHub <115622510+PhucHub@users.noreply.github.com>
2022-10-23 19:06:58 +0700

    Update Bai 1.cpp

--- hw/BTVN/Bai 1.cpp
+++ hw/BTVN/Bai 1.cpp
@@ -4,11 +4,11 @@ int main(){
 	printf("So da nhap  ");
 	scanf("%d",&n);
 	int S=0;
-	printf("Cac uoc so cua %d l‡: ",n);
+	printf("Cac uoc so cua %d l√†: ",n);
 	for(int i=1;i<=n;i++){
 		if(n%i==0){
 		S=S+i;
-		printf("%4d",i);
+		printf("%d",i);
 	}
 	}
      printf("\nTong cac uoc cua %d la %d",n,S);
