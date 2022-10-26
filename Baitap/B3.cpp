424fd46696456fae038da631d6dcadbdec519869
PhucHub <115622510+PhucHub@users.noreply.github.com>
2022-10-26 09:59:40 +0700

    BTVN

--- Baitap/B3.cpp
+++ Baitap/B3.cpp
@@ -1,6 +1,5 @@
 #include<stdio.h>
 #include"Thu vien.h"
- 
  int main(){
  	int n;
  	int d;
--- Baitap/Thu vien.h
+++ Baitap/Thu vien.h
@@ -14,3 +14,33 @@ bool ktSonguyento(int n){
 }
         return true;
 }
+int Maxtrong3so(int a,int b,int c){
+	if(a>b&&a>c){
+		return a;
+	}else{
+		if(b>c&&b>a){
+			return b;
+		}else{
+			return c;
+		}
+	}
+}
+
+int timsofibonaci(int n){
+	if(n<=0){
+		return 0;
+	}
+	if(n<3){
+		return 1;
+	}
+	    int x0=0;
+	    int x1=1;
+	    int x2=1;
+	    
+	    for(int i=3;i<=n;i++){
+		x0=x1;
+		x1=x2;
+		x2=x0+x1;
+	}
+      return x2;
+}
