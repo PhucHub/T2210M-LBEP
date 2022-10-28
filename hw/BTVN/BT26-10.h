28d4aa12ee50fa6dab23cce29220b355945c997c
PhucHub <115622510+PhucHub@users.noreply.github.com>
2022-10-28 08:14:41 +0700

    Update BT26-10.h

--- hw/BTVN/BT26-10.h
+++ hw/BTVN/BT26-10.h
@@ -40,10 +40,15 @@ bool Sochinhphuong(int a){
 	}
 }
 int Somindetongcacsolonhon10000(int n){
-	int S;
-    for(S=0,S<10000;n++){
-        S=S+n;
-}
+float S=0;
+ 	for(int i = 1; i <= n; i++){
+    S+= 1.0 / i;
+  }
+  if(S<10000){
+  	S++;
+  }else{
+  	return S;
+  }
 }
 bool Sogiamdan(int n){
 	for(int i;i=n%10;){
