6af776641e00a2ab9a49bf941408ae337e5e8d95
PhucHub <115622510+PhucHub@users.noreply.github.com>
2022-10-31 11:38:35 +0700

    1

---
+++ hw/BTVN/Bai4.cpp
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main(){
+	int n;
+	scanf("%d",&n);
+	int arr[n];
+	printf("Nhap danh sach:\n");
+	for (int i=0;i<n;i++){
+		scanf("%d",&arr[i]);
+	}
+for (int i=n-1;i>=0;i--){
+	if(arr[i]%2!=0){
+		printf("So le cuoi cung la %d",arr[i]);
+		break;
+	}
+}
+}
